462b8ad2875dd2cbb7c03d0792e9f3b3fe3ba742
Pranav pendhare <pranavpendhare45@gmail.com>
2026-08-09 16:01:07 +0530

    Time: 7 ms (45.87%), Space: 42.1 MB (33.98%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    static public int fib(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fib(n - 1) + fib(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        System.out.println(fib(n));
+    }
+}
